84ed4ff42d647a003cbaa1f36f610a997e2e00a0
Bich Ngoc <100981252+cogn99@users.noreply.github.com>
2025-12-06 16:37:20 +0700

    Update tpk.user.js

--- tpk.user.js
+++ tpk.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name         Rophim Full VIP
+// @name         RFV
 // @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  Bypass VIP + Coin trên Rophim
-// @author       FireT
-// @require      https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/rophim_vip.js
-// @homepageURL   https://github.com/firetofficial/bypass-rophim-vip
-// @downloadURL   https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
-// @updateURL     https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
+// @version      2.0
+// @description  RFV
+// @author       COGNI
+// @require      https://raw.githubusercontent.com/cogn99/rpt/refs/heads/main/rp.js
+// @homepageURL   https://github.com/cogn99/rpt
+// @downloadURL   https://raw.githubusercontent.com/cogn99/rpt/refs/heads/main/tpk.user.js
+// @updateURL     https://raw.githubusercontent.com/cogn99/rpt/refs/heads/main/tpk.user.js
 // @match        *://www.rophim.me/*
 // @match        *://www.rophim.mx/*
 // @match        *://www.rophim.li/*
 // @run-at       document-start
 // @grant        none
-// @icon         https://avatars.githubusercontent.com/u/120646974?s=48&v=4
+// @icon         https://avatars.githubusercontent.com/u/100981252?s=48&v=4
 // ==/UserScript==
